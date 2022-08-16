a1e9793cc7b7c3ec3bf748cc23b2f8f87ac7af15
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-08-17 01:24:41 +1200

    testing sass

---
+++ react-dashboard/src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './home.scss'
+
+export const Home = () => {
+  return (
+    <div className='home'>
+      <h1 className='title'>Lama Dev</h1>
+    </div>
+  )
+}
